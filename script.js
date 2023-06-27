20f4412965518c8f8dd9b6faa728d777a74e4fec
skyviolet <mloida591@gmail.com>
2023-06-27 16:07:21 -0400

    estilos

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const outcomeDiv =document.querySelector(".outcome")
+const outcomeDiv = document.querySelector(".outcome")
 const playerScoreSpan = document.querySelector(".player-score")
 const computerScoreSpan = document.querySelector(".computer-score")
 const buttons = document.querySelectorAll("button")
-const options = ["paper", "rock", "scissor"]
+const options = ["✋ paper", "✊ rock", "✌ scissor"]
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random()*options.length)];
@@ -12,14 +12,16 @@ function getComputerChoice(){
 let computerScore = 0;
 let playerScore = 0;
 function winner(playerSelection, computerSelection){
+    const h3 = document.createElement("h3")
+    h3.innerText = `Computer Selection: ${computerSelection}`
     const p = document.createElement("p")
     if (playerSelection === computerSelection) {
         p.innerText = `It's a tie! You both picked ${playerSelection}`
     }
     else if (
-        (playerSelection === "rock" && computerSelection === "scissor") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissor" && computerSelection === "paper")
+        (playerSelection === "✊ rock" && computerSelection === "✌ scissor") ||
+        (playerSelection === "✋ paper" && computerSelection === "✊ rock") ||
+        (playerSelection === "✌ scissor" && computerSelection === "✋ paper")
      ){
         playerScore ++
         p.innerText = `You won! ${playerSelection} wins ${computerSelection}`
@@ -28,6 +30,7 @@ function winner(playerSelection, computerSelection){
         computerScore ++
         p.innerText = `You lost! ${computerSelection} wins ${playerSelection}`
      }
+     outcomeDiv.appendChild(h3)
      outcomeDiv.appendChild(p)
 }
 
@@ -35,11 +38,11 @@ const checkForWinner = (playerScore, computerScore) => {
     const h2 =document.createElement("h2")
     if (playerScore === 3){
         h2.classList.add("player-won")
-        h2.innerText = `You won ${playerScore} to ${computerScore}`
+        h2.innerText = `You won ${playerScore} to ${computerScore}!!`
     }
     else if (computerScore === 3 ){
         h2.classList.add("computer-won")
-        h2.innerText = `You lost ${playerScore} to ${computerScore}`
+        h2.innerText = `You lost ${playerScore} to ${computerScore}!!`
     }
     outcomeDiv.append(h2)
 }
